bb1f20d2ca710c491e4e7c345306a99d8be26f96
pavel.sher <pavel.sher@jetbrains.com>
2018-01-18 22:39:24 +0100

    add perfmon feature

--- .teamcity/Sandbox_TeamcityBranchesTest/buildTypes/Sandbox_TeamcityBranchesTest_Build.kt
+++ .teamcity/Sandbox_TeamcityBranchesTest/buildTypes/Sandbox_TeamcityBranchesTest_Build.kt
@@ -1,8 +1,7 @@
 package Sandbox_TeamcityBranchesTest.buildTypes
 
-import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
 
 object Sandbox_TeamcityBranchesTest_Build : BuildType({
     uuid = "45af33dd-61ca-4739-9430-e4a989f43f6b"
@@ -28,5 +27,11 @@ object Sandbox_TeamcityBranchesTest_Build : BuildType({
 
     requirements {
         contains(param = "teamcity.agent.jvm.os.name", value = "Linux")
+    }
+
+    features {
+        feature {
+            type = "perfmon"
+        }
     }
 })
