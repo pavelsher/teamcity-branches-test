29da861c1dfbc19403364ab1b93b42c39915a9e5
pavel.sher <pavel.sher@jetbrains.com>
2018-01-19 00:25:50 +0100

    add custom failure condition

--- .teamcity/Sandbox_TeamcityBranchesTest/buildTypes/Sandbox_TeamcityBranchesTest_Build.kt
+++ .teamcity/Sandbox_TeamcityBranchesTest/buildTypes/Sandbox_TeamcityBranchesTest_Build.kt
@@ -2,6 +2,8 @@ package Sandbox_TeamcityBranchesTest.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2017_2.failureConditions.BuildFailureOnText
+import jetbrains.buildServer.configs.kotlin.v2017_2.failureConditions.failOnText
 
 object Sandbox_TeamcityBranchesTest_Build : BuildType({
     uuid = "45af33dd-61ca-4739-9430-e4a989f43f6b"
@@ -41,6 +43,15 @@ object Sandbox_TeamcityBranchesTest_Build : BuildType({
     features {
         feature {
             type = "perfmon"
+        }
+    }
+
+    failureConditions {
+        failOnText {
+            conditionType = BuildFailureOnText.ConditionType.CONTAINS
+            pattern = "This is a"
+            failureMessage = "Error!"
+            reverse = false
         }
     }
 })
